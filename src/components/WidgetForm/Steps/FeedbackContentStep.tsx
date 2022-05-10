15c6f0be7d7c3e72fa3de6dec2d3ff3edeb5a153
Leonardo Brizolla <leonardobrizollars@gmail.com>
2022-05-10 16:35:00 -0300

    Page success

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -7,42 +7,52 @@ import { ScreenshotButton } from "../ScreenshotButton";
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
   onFeedbackRestartRequested: () => void;
+  onFeedbackSent: () => void;
 }
 
-export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
+export function FeedbackContentStep({
+  feedbackType,
+  onFeedbackRestartRequested,
+  onFeedbackSent,
+}: FeedbackContentStepProps) {
   const feedbackTypeInfo = feedbackTypes[feedbackType];
   const [screenshot, setScreenshot] = useState<string | null>(null);
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
 
   function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    console.log({comment, screenshot});
+    console.log({ comment, screenshot });
+    onFeedbackSent();
   }
-  
+
   return (
     <>
-    <header>
-      <button 
-      type="button" 
-      className='absolute top-5 left-5 text-zinc-400 hover:text-zinc-100'
-      onClick={onFeedbackRestartRequested}
-      >
-        <ArrowLeft weight="bold" className='w-4 h-4'/>
-      </button>
+      <header>
+        <button
+          type="button"
+          className="absolute top-5 left-5 text-zinc-400 hover:text-zinc-100"
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
 
-      <span className='text-xl leading-6 flex items-center gap-2'>
-        <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className='w-6 h-6'/>
-        {feedbackTypeInfo.title}
-      </span>
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+            className="w-6 h-6"
+          />
+          {feedbackTypeInfo.title}
+        </span>
 
-      <CloseButton />
-    </header>   
+        <CloseButton />
+      </header>
 
-    <form onSubmit={handleSubmitFeedback} className='my-4 w-full'>
-      <textarea 
-        placeholder='Conte com detalhes o que esta acontecendo...'
-        onChange={(event) => setComment(event.target.value)}
-        className='
+      <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
+        <textarea
+          placeholder="Conte com detalhes o que esta acontecendo..."
+          onChange={(event) => setComment(event.target.value)}
+          className="
           min-w-[304px]
           w-full
           min-h-[112px]
@@ -61,23 +71,24 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
           scrollbar-thumb-zinc-700
           scrollbar-track-transparent
           scrollbar-thin
-        '
-      />
-
-      <footer className='flex gap-2 mt-2'>
-        <ScreenshotButton
-          screenshot={screenshot}
-          onScreenshotTook={setScreenshot}
+        "
         />
-        
-        <button
-          type='submit'
-          className='p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors'
-        >
-          Enviar feedback
-        </button>
-      </footer>
-    </form>
-  </>
-  )
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
+
+          <button
+            type="submit"
+            disabled={!comment}
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
+    </>
+  );
 }
